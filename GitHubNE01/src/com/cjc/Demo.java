5b32a72a724f51ff638f9d270d95f5aa1b6af113
AbhayaChavan <123442189+AbhayaChavan@users.noreply.github.com>
2023-01-25 12:05:49 +0530

    By Abhaya

--- GitHubNE01/src/com/cjc/Demo.java
+++ GitHubNE01/src/com/cjc/Demo.java
@@ -2,6 +2,11 @@ package com.cjc;
 
 public class Demo
 {
+	public void m1()
+	{
+		System.out.println("Hello Github");
+	}
+	
    public static void main(String args[])
    {
 	   System.out.println("Hello");
